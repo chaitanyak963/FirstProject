cc9ebd4a5d8ab3b9ee63e9012e6d29049ff7ea55
chaitanyak963 <chaitanyakarri963@gmail.com>
2019-01-18 10:27:56 +0530

    added

--- app/src/main/java/com/example/venky/firstproject/MainActivity.java
+++ app/src/main/java/com/example/venky/firstproject/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         firstRecyclerview = findViewById(R.id.myrecylerview);
-    
+
         int[] movie_posters ={
           R.drawable.kgf,
                 R.drawable.aravindha,
